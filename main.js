07f80f3b486356f1d52238c5a271fdef400e2987
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-11-13 15:47:52 -0500

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let delay = setTimeout(message, 2000);
+
+function message(){
+    document.getElementById("demo").innerText = "THANK YOU FOR WATCHING!";
+    document.getElementById("spinner").style.display = "none";
+}
